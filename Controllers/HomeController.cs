186c47fd975253a455bd8207f4f936dff241e646
Dmitry Melnikov <suwako.moria@gmail.com>
2023-07-10 22:05:16 +0300

    You can get input device by info (USB, for example)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
         Utilities.CommandLine bash = new Utilities.CommandLine();
         Audio.FFMpegSettings newSettings = new Audio.FFMpegSettings();
         var inputDevices = bash.GetAudioDevices();
-        Audio.Recording recordingObj = new Audio.Recording(inputDevices.Where(x => x.deviceId == "1").First(), newSettings);
+        Audio.Recording recordingObj = new Audio.Recording(inputDevices.Where(x => x.deviceInfo.Contains("USB")).First(), newSettings);
         var file = recordingObj.RecordAudio();
         Audio.AudioProcessing audio = new Audio.AudioProcessing("/Recordings");
         audio.ProcessAudio(file);
