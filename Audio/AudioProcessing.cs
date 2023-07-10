edabd4fdb9158b3221fe026cf391593c8612bd9c
Dmitry Melnikov <suwako.moria@gmail.com>
2023-07-10 22:41:33 +0300

    Moved to async methods

--- Audio/AudioProcessing.cs
+++ Audio/AudioProcessing.cs
@@ -9,23 +9,19 @@ namespace Bird_Box.Audio
     {
         string pathToAudio {get; set;}
         string minConfidence {get; set;} = "0.3";
-        string cpuThreads { get; set; } = "1";
+        public string cpuThreads { get; set; } = "1";
         public AudioProcessing(string recordingsPath)
         {
             pathToAudio = recordingsPath;
         }
-        public bool ProcessAudio(string fileName)
+        public async Task<bool> ProcessAudioAsync(string fileName)
         {
-            string processOutput;
             var processInfo = new System.Diagnostics.ProcessStartInfo();
             processInfo.FileName = "/bin/bash";
             processInfo.Arguments = $"-c \"python3 BirdNET-Analyzer/analyze.py --threads {cpuThreads} --min_conf {minConfidence} --i Recordings/{fileName}.wav --o /home/m918/Bird-Box/Recordings/{fileName}-result.txt";
             processInfo.RedirectStandardOutput = true;
-            using (var process = System.Diagnostics.Process.Start(processInfo))
-            {
-                processOutput = process.StandardOutput.ReadToEnd();
-            }
-            Console.WriteLine(processOutput);
+            var process = System.Diagnostics.Process.Start(processInfo);
+            await process.WaitForExitAsync();
             return true;
         }
     }
