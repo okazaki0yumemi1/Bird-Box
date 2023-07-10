c0bd017ff3de7dd3fe17d4fc253fa4c681124d6e
Dmitry Melnikov <suwako.moria@gmail.com>
2023-07-10 10:20:40 +0300

    Create new Audio folder and Settings struct

---
+++ Audio/FFMpegSettings.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bird_Box.Audio
+{
+    public struct FFMpegSettings
+    {
+        public string ffmpegExecutable {get; private set;}
+        public string outputPath {get; set;}
+        public void FindFFmpegExecPath()
+        {
+            var processOutput = "";
+            var processInfo = new System.Diagnostics.ProcessStartInfo();
+            processInfo.FileName = "/bin/bash";
+            //Execute find ffmpeg command, then get columns from second to last
+            processInfo.Arguments = "-c \"whereis ffmpeg | awk '{$1=\"\"; print $0}' \"";
+            processInfo.RedirectStandardOutput = true;
+            using (var process = System.Diagnostics.Process.Start(processInfo))
+            {
+                processOutput = process.StandardOutput.ReadToEnd();
+            }
+            var paths = processOutput.Split(" ").ToList();
+            foreach (var path in paths)
+            {
+                //Execute ffmpeg path --version , then get first lind, first column.
+                //If ffmpeg is available, it should print "ffmpeg"
+                processInfo.Arguments = $"-c \"{path} -version | head -n 1  | awk '{{print $1}}'\"";
+                processInfo.RedirectStandardOutput = true;
+                var process = System.Diagnostics.Process.Start(processInfo);
+                processOutput = process.StandardOutput.ReadToEnd();
+                if (processOutput == "ffmpeg") 
+                {
+                    ffmpegExecutable = processOutput;
+                    return;
+                }  
+            }
+        }
+        public void SetFFmpegExecPath(string newPath)
+        {
+            ffmpegExecutable = newPath;
+        }
+        public FFMpegSettings()
+        {
+            this.outputPath = "./recordings";
+            this.ffmpegExecutable = "ffmpeg";
+        }
+    }
+}
