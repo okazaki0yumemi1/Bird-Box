5f16fcc4c6c2bbc3a5755593a9f4738e5feec6e3
Dmitry Melnikov <suwako.moria@gmail.com>
2023-07-11 07:17:17 +0300

    Confidence option added

--- Audio/AudioProcessing.cs
+++ Audio/AudioProcessing.cs
@@ -8,7 +8,7 @@ namespace Bird_Box.Audio
     public class AudioProcessing
     {
         string pathToAudio {get; set;}
-        string minConfidence {get; set;} = "0.3";
+        string minConfidence {get; set;} = "0.5";
         public string cpuThreads { get; set; } = "1";
         public AudioProcessing(string recordingsPath)
         {
@@ -30,13 +30,17 @@ namespace Bird_Box.Audio
         }
         public async Task<bool> ProcessAudioAsync(string fileName)
         {
+            string processOutput = "";
             var processInfo = new System.Diagnostics.ProcessStartInfo();
             processInfo.FileName = "/bin/bash";
-            processInfo.Arguments = $"-c \"python3 BirdNET-Analyzer/analyze.py --threads {cpuThreads} --min_conf {minConfidence} --i Recordings/{fileName}.wav --o /home/m918/Bird-Box/Recordings/{fileName}-result.txt";
+            processInfo.Arguments = $"-c \"python3 BirdNET-Analyzer/analyze.py --min_conf {minConfidence} --threads {cpuThreads.Replace("\n", "")} --i Recordings/{fileName} --o /home/m918/Bird-Box/Recordings/{fileName}-result.txt";
             processInfo.RedirectStandardOutput = true;
-            var process = System.Diagnostics.Process.Start(processInfo);
-            await process.WaitForExitAsync();
-            return true;
+            using (var process = System.Diagnostics.Process.Start(processInfo))
+            {
+                processOutput = process.StandardOutput.ReadToEnd();
+            }
+            if (processOutput.Contains("Finished")) return true;
+            else return false;
         }
     }
 }
