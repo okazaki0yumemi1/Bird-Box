45af43641c006d5ed24515190fc9812015d24448
Dmitry Melnikov <suwako.moria@gmail.com>
2023-07-12 20:51:35 +0300

    Migrations

--- Migrations/20230712161316_dateTimeAddition.Designer.cs
+++ Migrations/20230712161316_dateTimeAddition.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bird_Box.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bird_Box.Migrations
 {
     [DbContext(typeof(BirdBoxContext))]
-    partial class BirdBoxContextModelSnapshot : ModelSnapshot
+    [Migration("20230712161316_dateTimeAddition")]
+    partial class dateTimeAddition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
