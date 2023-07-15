1f925065499e5258eef0dac04f36e224a6c70a89
Dmitry Melnikov <suwako.moria@gmail.com>
2023-07-15 16:38:30 +0300

    Install script fixes

--- InstallScript.sh
+++ InstallScript.sh
@@ -15,3 +15,4 @@ apt-get install -y dotnet-sdk-7.0 && echo "Dotnet sdk install was successfull" |
 echo "Creating directory for SQLite database"
 mkdir ./Data || echo "Unable to create directory for database, please create folder "Data" in Bird-Box folder"
 mkdir ./Recordings || echo "Unable to create directory for recording results, please create folder "Recordings" in Bird-Box folder"
+echo "You also have to download BirdNET-Analyzer model and put it in the same folder as ./Bird-Box file (same as "Data" and "Recording" folders)"
