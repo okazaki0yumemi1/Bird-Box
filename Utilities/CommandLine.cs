03dba5764c66fe8a3653ddadc5ca5e85455d91da
Okazaki Yumemi <suwako.moria@gmail.com>
2023-09-11 21:46:41 +0300

    CSharpier

--- Utilities/CommandLine.cs
+++ Utilities/CommandLine.cs
@@ -7,32 +7,37 @@ namespace Bird_Box.Utilities
 {
     public class CommandLine
     {
-        public CommandLine() { }
-
+        public CommandLine(){}
+        
+        /// <summary>
+        /// Get list of audio input devices.
+        /// </summary>
+        /// <returns>List of audio devices: device Id, device info</returns>
         public List<Audio.Microphone> GetAudioDevices()
         {
             var devices = new List<Audio.Microphone>();
             var result = ExecuteCommand("arecord -l | grep -o 'card [0-99]'").Replace("card ", "");
-            if (String.IsNullOrEmpty(result))
-                return devices;
+            if (String.IsNullOrEmpty(result)) return devices;
             var lines = result.Split("\n");
             foreach (var line in lines)
             {
-                if (line == "")
-                    return devices;
+                if (line == "") return devices;
                 line.Trim();
-                string info = ExecuteCommand($"arecord -l | grep 'card {line}'")
-                    .Replace(Environment.NewLine, "");
+                string info = ExecuteCommand($"arecord -l | grep 'card {line}'").Replace(Environment.NewLine, "");
                 var devId = ExecuteCommand($"arecord -l | grep -o 'card {line}' | grep -o '[0-9]'");
                 devId = devId.Replace(Environment.NewLine, "");
-                devId = ExecuteCommand($"arecord -l | grep -o 'card {line}' | grep -o '[1-9]'")
-                    .Replace(Environment.NewLine, "");
+                devId = ExecuteCommand($"arecord -l | grep -o 'card {line}' | grep -o '[1-9]'").Replace(Environment.NewLine, "");
                 devices.Add(new Audio.Microphone(devId, info));
                 //Test devices here!
             }
             return devices;
         }
 
+        /// <summary>
+        /// Run a bash command asynchroniously.
+        /// </summary>
+        /// <param name="parameters">bash command</param>
+        /// <returns>Command output</returns>
         public async Task<string> ExecuteCommandAsync(string parameters)
         {
             Task<string> processOutput;
@@ -47,6 +52,11 @@ namespace Bird_Box.Utilities
             return processOutput.Result;
         }
 
+                /// <summary>
+        /// Run a bash command.
+        /// </summary>
+        /// <param name="parameters">bash command</param>
+        /// <returns>Command output</returns>
         public string ExecuteCommand(string parameters)
         {
             string processOutput;
--- Utilities/RecordingSchedule.cs
+++ Utilities/RecordingSchedule.cs
@@ -10,21 +10,24 @@ namespace Bird_Box.Utilities
     public class RecordingSchedule
     {
         TimeSpan timer { get; set; }
-        Queue<string> UnprocessedRecordings { get; set; } = new Queue<string>();
-        List<Task> ProcessingAudio { get; set; } = new List<Task>();
-        string recordingsPath { get; set; } = "Recordings";
-
+        Queue<string> UnprocessedRecordings {get; set;} = new Queue<string>();
+        List<Task> ProcessingAudio {get; set;} = new List<Task>();
+        string recordingsPath { get; set;} = "Recordings";
         public RecordingSchedule(TimeSpan timespan)
         {
             timer = timespan;
         }
-
         public RecordingSchedule(TimeSpan timespan, string resultsPath)
         {
             timer = timespan;
             recordingsPath = resultsPath;
         }
-
+        
+        /// <summary>
+        /// This is a task responsible for recording and analysing audio recordings.
+        /// </summary>
+        /// <param name="options">BirdNET Analyzer parameters</param>
+        /// <returns></returns>
         public async Task<int> RecordAndRecognize(AnalyzerOptions options)
         {
             //var secondsElapsed = new TimeSpan(0, 0, 0);
@@ -42,41 +45,54 @@ namespace Bird_Box.Utilities
                 Record();
                 recordingsMade++;
                 ProcessingAudio.Add(RecognizeBird(options));
-                if ((recordingsMade * 10) >= timer.TotalSeconds)
-                    break;
+                if ((recordingsMade*10) >= timer.TotalSeconds) break;
             }
             return recordingsMade;
         }
-
+        /// <summary>
+        /// Starts recording via USB input device.
+        /// </summary>
         public void Record()
         {
             Utilities.CommandLine bash = new Utilities.CommandLine();
             Audio.FFMpegSettings newSettings = new Audio.FFMpegSettings();
             var inputDevices = bash.GetAudioDevices();
-            Audio.Recording recordingObj = new Audio.Recording(
-                inputDevices.Where(x => x.deviceInfo.Contains("USB")).FirstOrDefault()
-                    ?? inputDevices.FirstOrDefault(),
-                newSettings
-            );
+            Audio.Recording recordingObj = new Audio.Recording(inputDevices.Where(x => x.deviceInfo.Contains("USB")).FirstOrDefault() ?? inputDevices.FirstOrDefault(), newSettings);
             UnprocessedRecordings.Enqueue(recordingObj.RecordAudio());
         }
 
+        /// <summary>
+        /// This method lets you choose mic by entering device id.
+        /// </summary>
+        /// <param name="deviceId">input device id. You can see it by running "arecord -l" in terminal</param>
+        public void Record(string deviceId)
+        {
+            Utilities.CommandLine bash = new Utilities.CommandLine();
+            Audio.FFMpegSettings newSettings = new Audio.FFMpegSettings();
+            var inputDevices = bash.GetAudioDevices();
+            Audio.Recording recordingObj = new Audio.Recording(inputDevices.Where(x => x.deviceId == deviceId).FirstOrDefault(), newSettings);
+            UnprocessedRecordings.Enqueue(recordingObj.RecordAudio());
+        }
+
+        /// <summary>
+        /// Starts analysis of recording with passed parameters. See BirdNET Analyzer documentation for details.
+        /// </summary>
+        /// <param name="options">BirdNET Analyzer parameters</param>
+        /// <returns></returns>
         public Task<bool> RecognizeBird(AnalyzerOptions options)
         {
             Audio.AudioProcessing audio = new Audio.AudioProcessing(recordingsPath, options);
             double confidenceInput = 0;
-            if (Double.TryParse(options.minimumConfidence, out confidenceInput))
+            if (Double.TryParse(options.minimumConfidence, out confidenceInput)) 
             {
                 if ((confidenceInput < 1) && (confidenceInput > 0.01))
                 {
                     options.minimumConfidence = confidenceInput.ToString();
                 }
-                else
-                    options.minimumConfidence = 0.75d.ToString();
+                else options.minimumConfidence = 0.75d.ToString();
             }
-            else
-                options.minimumConfidence = 0.75d.ToString();
-
+            else options.minimumConfidence = 0.75d.ToString();
+            
             return audio.ProcessAudioAsync(UnprocessedRecordings.Dequeue());
         }
     }
