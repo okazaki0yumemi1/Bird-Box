49bcc41232ae1ba519c738b71a7a0d7d09779f0c
Okazaki Yumemi <suwako.moria@gmail.com>
2023-10-07 08:31:16 +0300

    Add a code to record with chosen microphone, not fully implemented though

--- Audio/Recording.cs
+++ Audio/Recording.cs
@@ -21,11 +21,32 @@ namespace Bird_Box.Audio
         {
             var fileName = DateTime.Now.ToString("yyyy'-'MM'-'dd'-'HH'-'mm'-'ss"); //DateTime.Now.ToString();
             var outputFile = settings.outputPath;
-            var result = ExecuteCommand(
-                $"{settings.ffmpegExecutable} -f pulse -i default -t 10 {settings.outputPath}/{fileName}.wav"
-            );
-            return fileName + ".wav";
+            if (inputDevice is not null)
+            {
+                var result = ExecuteCommand(
+                    $"{settings.ffmpegExecutable} -f pulse -i hw:{inputDevice.deviceId} -t 10 {settings.outputPath}/{fileName}.wav"
+                );
+                return fileName + ".wav";
+            }
+            else 
+            {
+                var result = ExecuteCommand(
+                    $"{settings.ffmpegExecutable} -f pulse -i default -t 10 {settings.outputPath}/{fileName}.wav"
+                );
+                return fileName + ".wav";
+            }
+            throw new NotImplementedException("No microphone to record with! Check arecord -l for any input devices.")
         }
+
+        // public string RecordAudio(Microphone inputDevice)
+        // {
+        //     var fileName = DateTime.Now.ToString("yyyy'-'MM'-'dd'-'HH'-'mm'-'ss"); //DateTime.Now.ToString();
+        //     var outputFile = settings.outputPath;
+        //     var result = ExecuteCommand(
+        //         $"{settings.ffmpegExecutable} -f pulse -i hw:{inputDevice.deviceId} -t 10 {settings.outputPath}/{fileName}.wav"
+        //     );
+        //     return fileName + ".wav";
+        // }
 
         string ExecuteCommand(string parameters)
         {
