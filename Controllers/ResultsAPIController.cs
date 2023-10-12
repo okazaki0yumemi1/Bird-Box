6dedabc68957b62f2852a69b404c583513efd915
Okazaki Yumemi <suwako.moria@gmail.com>
2023-10-12 08:04:01 +0300

    Changed Controller routing and naming

---
+++ Controllers/ResultsAPIController.cs
@@ -0,0 +1,212 @@
+using System.Text.Json;
+using Bird_Box.Data;
+using Bird_Box.Models;
+using Bird_Box.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bird_Box.Controllers
+{
+    [ApiController]
+    public class ResultsAPIController : ControllerBase
+    {
+        Task ListeningTask;
+        private readonly BirdRepository _dbOperations;
+        private readonly AnalyzerOptions _defaultOptions;
+        private readonly IConfigurationRoot _config;
+
+        public ResultsAPIController(BirdRepository dbOperations)
+        {
+            _dbOperations = dbOperations;
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Get values from the config given their key and their target type.
+            _defaultOptions = _config
+                .GetRequiredSection("BirdNETOptions:Default")
+                .Get<AnalyzerOptions>();
+        }
+
+        [HttpGet("api/results/{recordId}")]
+        public async Task<IActionResult> GetByID([FromRoute] string recordId)
+        {
+            var bird = _dbOperations.GetByGuid(recordId);
+            if (bird == null)
+                return NotFound();
+            return Ok(bird);
+        }
+
+        [HttpGet("api/results")]
+        public async Task<IActionResult> GetAll()
+        {
+            var results = await _dbOperations.GetAll();
+            results.OrderByDescending(x => x.recodingDate);
+            return Ok(results);
+        }
+
+        [HttpGet("api/results/byDay/{yyyyMMdd}")]
+        public async Task<IActionResult> GetAllDetectionsByDay([FromRoute] DateTime yyyyMMdd)
+        {
+            DateTime date = yyyyMMdd;
+            //DateTime.TryParse(yyyyMMdd, out date);
+            var results = await _dbOperations.GetByDate(date);
+            results.OrderByDescending(x => x.recodingDate.Ticks);
+            return Ok(results);
+        }
+
+        [HttpGet("api/results/byBird/{birdName}")]
+        public async Task<IActionResult> GetBirdByName([FromRoute] string birdName)
+        {
+            var records = _dbOperations.GetByBirdName(birdName);
+            if (records == null)
+                return NotFound();
+            return Ok(records);
+        }
+
+        [HttpPost("api/results/start/{hours}")]
+        public async Task<IActionResult> StartRecording(
+            [FromBody] AnalyzerOptions? optionsInput,
+            [FromRoute] string hours
+        )
+        {
+            TimeSpan _hours;
+            var options = new AnalyzerOptions(setWeek: true);
+            if (optionsInput is null)
+            {
+                options = _defaultOptions;
+            }
+            else
+            {
+                options = ValidModel(optionsInput);
+                if (options is null)
+                    options = _defaultOptions;
+            }
+
+            if (!TimeSpan.TryParse(hours, out _hours))
+                _hours = TimeSpan.FromHours(1); //default value - 1 hour
+            Utilities.RecordingSchedule scheduleRecording = new Utilities.RecordingSchedule(_hours);
+            ListeningTask = scheduleRecording.RecordAndRecognize(options);
+            return Ok(
+                $"The task will be run for {_hours} hours.{Environment.NewLine} The options are:{Environment.NewLine} {JsonSerializer.Serialize(options)}"
+            );
+        }
+
+        AnalyzerOptions ValidModel(AnalyzerOptions inputModel)
+        {
+            var result = new AnalyzerOptions();
+            if ((inputModel.latitude is not null) && (inputModel.longitude is not null))
+            {
+                int la = -1;
+                int lo = -1;
+                if (
+                    int.TryParse(inputModel.latitude, out la)
+                    && (int.TryParse(inputModel.longitude, out lo))
+                )
+                {
+                    if (((la >= 0) && (lo <= 60)) && ((la >= 0) && (lo <= 60)))
+                    {
+                        result.latitude = la.ToString();
+                        result.longitude = lo.ToString();
+                    }
+                    else
+                    {
+                        result.latitude = "-1";
+                        result.longitude = "-1";
+                    }
+                }
+            }
+            if (inputModel.weekOfTheYear is not null)
+            {
+                int w = 0;
+                if (int.TryParse(inputModel.weekOfTheYear, out w))
+                {
+                    if ((w >= 1) && (w <= 48))
+                        result.weekOfTheYear = w.ToString();
+                    else if (w == -1)
+                        result.weekOfTheYear = w.ToString();
+                }
+                else
+                    result.weekOfTheYear = ((int)DateTime.Now.Day / 7).ToString();
+            }
+            if (inputModel.sensitivity is not null)
+            {
+                float s = 1;
+                if (float.TryParse(inputModel.sensitivity, out s))
+                {
+                    if ((s >= 0.5) || (s <= 1.5))
+                        result.sensitivity = s.ToString();
+                }
+            }
+            if (inputModel.minimumConfidence is not null)
+            {
+                float c = 0.01f;
+                if (float.TryParse(inputModel.minimumConfidence, out c))
+                {
+                    if ((c >= 0.01) || (c <= 0.99))
+                        result.minimumConfidence = c.ToString();
+                }
+            }
+            if (inputModel.overlapSegments is not null)
+            {
+                float oS = 0.0f;
+                if (float.TryParse(inputModel.sensitivity, out oS))
+                {
+                    if ((oS >= 0.01) || (oS < 1))
+                        result.sensitivity = oS.ToString();
+                }
+            }
+            if (inputModel.cpuThreads is not null)
+            {
+                int t = 1;
+                if (int.TryParse(inputModel.cpuThreads, out t))
+                {
+                    if ((t >= 1) || (t < 768))
+                        result.cpuThreads = t.ToString();
+                }
+            }
+            if (inputModel.processingBatchSize is not null)
+            {
+                int pBS = 1;
+                if (int.TryParse(inputModel.sensitivity, out pBS))
+                {
+                    if ((pBS >= 0.01) || (pBS < 1))
+                        result.sensitivity = pBS.ToString();
+                }
+            }
+            if (inputModel.locale is not null)
+            {
+                if (inputModel.Locales.Contains(inputModel.locale))
+                    result.locale = inputModel.locale;
+            }
+            if (inputModel.speciesFrequencyThreshold is not null)
+            {
+                float sf_thresh = 0.03f;
+                if (float.TryParse(inputModel.speciesFrequencyThreshold, out sf_thresh))
+                {
+                    if ((sf_thresh >= 0.01) || (sf_thresh <= 0.99))
+                        result.speciesFrequencyThreshold = sf_thresh.ToString();
+                }
+            }
+            return result;
+        }
+
+        [HttpGet("api/results/process")]
+        public async Task<IActionResult> ProcessResults()
+        {
+            RecognitionResultsProcessing rrp = new RecognitionResultsProcessing("Recordings/");
+            var results = rrp.ProcessAllFiles();
+            return Ok($"Added {_dbOperations.CreateRange(results)} results");
+        }
+
+        [HttpDelete("api/results/{recordId}")]
+        public async Task<IActionResult> DeleteById([FromRoute] string recordId)
+        {
+            var deletedItems = await _dbOperations.DeleteById(recordId);
+            if (deletedItems > 0)
+                return Ok("Record deleted successfully");
+            else
+                return NoContent();
+        }
+    }
+}
--- Controllers/ResultsController.cs
+++ Controllers/ResultsController.cs
@@ -1,212 +1,87 @@
-using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Bird_Box.Data;
-using Bird_Box.Models;
-using Bird_Box.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Bird_Box.Controllers
 {
-    [ApiController]
-    public class ResultsController : ControllerBase
+    [ApiExplorerSettings(IgnoreApi=true)]
+    public class ResultsController : Controller
     {
-        Task ListeningTask;
+        private readonly ILogger<ResultsController> _logger;
         private readonly BirdRepository _dbOperations;
-        private readonly AnalyzerOptions _defaultOptions;
-        private readonly IConfigurationRoot _config;
 
-        public ResultsController(BirdRepository dbOperations)
+        public ResultsController(ILogger<ResultsController> logger, BirdRepository dbOperations)
         {
             _dbOperations = dbOperations;
-            _config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .Build();
-
-            // Get values from the config given their key and their target type.
-            _defaultOptions = _config
-                .GetRequiredSection("BirdNETOptions:Default")
-                .Get<AnalyzerOptions>();
+            _logger = logger;
         }
 
-        [HttpGet("api/results/{recordId}")]
-        public async Task<IActionResult> GetByID([FromRoute] string recordId)
+        //[Route("~/")]
+        public IActionResult Index()
         {
-            var bird = _dbOperations.GetByGuid(recordId);
-            if (bird == null)
-                return NotFound();
-            return Ok(bird);
+            return View();
         }
 
-        [HttpGet("api/results")]
-        public async Task<IActionResult> GetAll()
+
+        //[Route("~/results")]
+        public async Task<IActionResult> ListAll()
         {
             var results = await _dbOperations.GetAll();
             results.OrderByDescending(x => x.recodingDate);
-            return Ok(results);
+            return View("Views/Results/Results.cshtml", results);
         }
 
-        [HttpGet("api/results/byDay/{yyyyMMdd}")]
-        public async Task<IActionResult> GetAllDetectionsByDay([FromRoute] DateTime yyyyMMdd)
+        [Route("~/error")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            DateTime date = yyyyMMdd;
-            //DateTime.TryParse(yyyyMMdd, out date);
-            var results = await _dbOperations.GetByDate(date);
-            results.OrderByDescending(x => x.recodingDate.Ticks);
-            return Ok(results);
+            return View("Error!");
         }
 
-        [HttpGet("api/results/byBird/{birdName}")]
-        public async Task<IActionResult> GetBirdByName([FromRoute] string birdName)
+        [HttpGet("Results/Delete/{objId}")]
+        public async Task<IActionResult> Delete([FromRoute] string objId)
         {
-            var records = _dbOperations.GetByBirdName(birdName);
-            if (records == null)
+
+            if (objId == null)
+            {
                 return NotFound();
-            return Ok(records);
+            }
+
+            var bird = await _dbOperations.GetByGuid(objId);
+            if (bird == null)
+            {
+                return NotFound();
+            }
+
+            return View(bird);
         }
-
-        [HttpPost("api/results/start/{hours}")]
-        public async Task<IActionResult> StartRecording(
-            [FromBody] AnalyzerOptions? optionsInput,
-            [FromRoute] string hours
-        )
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Route("Results/Delete/{objId}")]
+        public async Task<IActionResult> DeleteConfirmed([FromRoute] string objId)
         {
-            TimeSpan _hours;
-            var options = new AnalyzerOptions(setWeek: true);
-            if (optionsInput is null)
-            {
-                options = _defaultOptions;
-            }
-            else
-            {
-                options = ValidModel(optionsInput);
-                if (options is null)
-                    options = _defaultOptions;
-            }
-
-            if (!TimeSpan.TryParse(hours, out _hours))
-                _hours = TimeSpan.FromHours(1); //default value - 1 hour
-            Utilities.RecordingSchedule scheduleRecording = new Utilities.RecordingSchedule(_hours);
-            ListeningTask = scheduleRecording.RecordAndRecognize(options);
-            return Ok(
-                $"The task will be run for {_hours} hours.{Environment.NewLine} The options are:{Environment.NewLine} {JsonSerializer.Serialize(options)}"
-            );
-        }
-
-        AnalyzerOptions ValidModel(AnalyzerOptions inputModel)
-        {
-            var result = new AnalyzerOptions();
-            if ((inputModel.latitude is not null) && (inputModel.longitude is not null))
-            {
-                int la = -1;
-                int lo = -1;
-                if (
-                    int.TryParse(inputModel.latitude, out la)
-                    && (int.TryParse(inputModel.longitude, out lo))
-                )
-                {
-                    if (((la >= 0) && (lo <= 60)) && ((la >= 0) && (lo <= 60)))
-                    {
-                        result.latitude = la.ToString();
-                        result.longitude = lo.ToString();
-                    }
-                    else
-                    {
-                        result.latitude = "-1";
-                        result.longitude = "-1";
-                    }
-                }
-            }
-            if (inputModel.weekOfTheYear is not null)
-            {
-                int w = 0;
-                if (int.TryParse(inputModel.weekOfTheYear, out w))
-                {
-                    if ((w >= 1) && (w <= 48))
-                        result.weekOfTheYear = w.ToString();
-                    else if (w == -1)
-                        result.weekOfTheYear = w.ToString();
-                }
-                else
-                    result.weekOfTheYear = ((int)DateTime.Now.Day / 7).ToString();
-            }
-            if (inputModel.sensitivity is not null)
-            {
-                float s = 1;
-                if (float.TryParse(inputModel.sensitivity, out s))
-                {
-                    if ((s >= 0.5) || (s <= 1.5))
-                        result.sensitivity = s.ToString();
-                }
-            }
-            if (inputModel.minimumConfidence is not null)
-            {
-                float c = 0.01f;
-                if (float.TryParse(inputModel.minimumConfidence, out c))
-                {
-                    if ((c >= 0.01) || (c <= 0.99))
-                        result.minimumConfidence = c.ToString();
-                }
-            }
-            if (inputModel.overlapSegments is not null)
-            {
-                float oS = 0.0f;
-                if (float.TryParse(inputModel.sensitivity, out oS))
-                {
-                    if ((oS >= 0.01) || (oS < 1))
-                        result.sensitivity = oS.ToString();
-                }
-            }
-            if (inputModel.cpuThreads is not null)
-            {
-                int t = 1;
-                if (int.TryParse(inputModel.cpuThreads, out t))
-                {
-                    if ((t >= 1) || (t < 768))
-                        result.cpuThreads = t.ToString();
-                }
-            }
-            if (inputModel.processingBatchSize is not null)
-            {
-                int pBS = 1;
-                if (int.TryParse(inputModel.sensitivity, out pBS))
-                {
-                    if ((pBS >= 0.01) || (pBS < 1))
-                        result.sensitivity = pBS.ToString();
-                }
-            }
-            if (inputModel.locale is not null)
-            {
-                if (inputModel.Locales.Contains(inputModel.locale))
-                    result.locale = inputModel.locale;
-            }
-            if (inputModel.speciesFrequencyThreshold is not null)
-            {
-                float sf_thresh = 0.03f;
-                if (float.TryParse(inputModel.speciesFrequencyThreshold, out sf_thresh))
-                {
-                    if ((sf_thresh >= 0.01) || (sf_thresh <= 0.99))
-                        result.speciesFrequencyThreshold = sf_thresh.ToString();
-                }
-            }
-            return result;
-        }
-
-        [HttpGet("api/results/process")]
-        public async Task<IActionResult> ProcessResults()
-        {
-            RecognitionResultsProcessing rrp = new RecognitionResultsProcessing("Recordings/");
-            var results = rrp.ProcessAllFiles();
-            return Ok($"Added {_dbOperations.CreateRange(results)} results");
-        }
-
-        [HttpDelete("api/results/{recordId}")]
-        public async Task<IActionResult> DeleteById([FromRoute] string recordId)
-        {
-            var deletedItems = await _dbOperations.DeleteById(recordId);
+            var deletedItems = await _dbOperations.DeleteById(objId);
             if (deletedItems > 0)
-                return Ok("Record deleted successfully");
+                return Ok("Deleted successfully");//View("Views/Results/Delete.cshtml", deletedItems);
             else
                 return NoContent();
+            
         }
+
+        [Route("Results/Details/{objId}")]
+        public async Task<IActionResult> Details([FromRoute] string objId)
+        {
+            var bird = await _dbOperations.GetByGuid(objId);
+            if (bird == null)
+                return NoContent();
+            return View("Views/Results/Details.cshtml", bird);
+        }
+
     }
 }
