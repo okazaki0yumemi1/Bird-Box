0e8a845d7d7b5d547e99038f87290d8636a773b8
Okazaki Yumemi <suwako.moria@gmail.com>
2023-10-21 21:24:33 +0300

    Implement MicrophoneRepository for database operations

--- Data/MicrophoneRepository.cs
+++ Data/MicrophoneRepository.cs
@@ -2,11 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bird_Box.Audio;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bird_Box.Data
 {
-    public class MicrophoneRepository
+    public class MicrophoneRepository : IRepository<Microphone>
     {
-        
+        private readonly BirdBoxContext _context;
+
+        public MicrophoneRepository(BirdBoxContext context)
+        {
+            _context = context;
+        }
+
+        Task<int> IRepository<Microphone>.Create(Microphone inputDevice)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> IRepository<Microphone>.DeleteById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Microphone>> GetAll()
+        {
+            return await _context.InputDevices.ToListAsync();
+        }
+
+        public async Task<Microphone> GetByName(string deviceName)
+        {
+            return await _context.InputDevices.Where(x => x.deviceInfo.Contains(deviceName)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Microphone> GetById(string id)
+        {
+            return await _context.InputDevices.FirstOrDefaultAsync(x => x.objId == id);
+        }
     }
 }
