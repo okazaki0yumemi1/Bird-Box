bad6d17b6cd2c7d77ae9b973835b4af8a23f72a2
Okazaki Yumemi <suwako.moria@gmail.com>
2023-10-21 14:45:59 +0300

    Add new Model class DetectionModel

---
+++ Models/DetectionModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bird_Box.Audio;
+
+namespace Bird_Box.Models
+{
+    public class DetectionModel
+    {
+        public IdentifiedBird BirdDetection { get; private set; }
+        public Microphone InputDevice { get; private set; }
+
+        public DetectionModel(IdentifiedBird bird, Microphone inputDevice)
+        {
+            BirdDetection = bird;
+            InputDevice = inputDevice;
+        }
+    }
+}
