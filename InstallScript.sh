dc75a597475a5bad17800cef6db466efe18eb879
Okazaki Yumemi <suwako.moria@gmail.com>
2023-10-26 18:49:58 +0300

    Removed unused file

--- InstallScript.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-echo "Creating directory for SQLite database"
-mkdir ./Data || echo "Unable to create directory for database, please create folder "Data" in Bird-Box folder"
-mkdir ./Recordings || echo "Unable to create directory for recording results, please create folder "Recordings" in Bird-Box folder"
-echo "You also have to download BirdNET-Analyzer model and put it in the same folder as ./Bird-Box file (same as "Data" and "Recording" folders)"
