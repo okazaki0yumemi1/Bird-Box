cf0cb5a30526e0ecd1c5e3e51d91569f95936e80
Okazaki Yumemi <suwako.moria@gmail.com>
2024-03-07 22:09:36 +0300

    Update entity for EF Core migrations

--- Models/ListeningTask.cs
+++ Models/ListeningTask.cs
@@ -1,16 +1,18 @@
 using Bird_Box.Audio;
 using Bird_Box.Utilities;
-using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bird_Box.Models
 {
     public class ListeningTask : IEntity
     {
+        [Key]
         public string objId { get; init; } = Guid.NewGuid().ToString();
         public string OutputFolder { get; private set; }
         public int Hours { get; private set; }
         public Microphone InputDevice {  get; private set; } 
         public AnalyzerOptions Options { get; private set; }
+        public ListeningTask() { }
         public ListeningTask(string outputFolder, int hours, Microphone inputDevice, AnalyzerOptions options)
         {
             OutputFolder = outputFolder;
