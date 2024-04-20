7d37710d79caa2b984e19eb1ea5c169c6f9b55f6
Dmitry Melnikov <suwako.moria@gmail.com>
2024-04-20 22:45:52 +0300

    Set channels back to 1

--- Audio/Recording.cs
+++ Audio/Recording.cs
@@ -28,7 +28,7 @@ namespace Bird_Box.Audio
             if (inputDevice is not null)
             {
                 var result = CommandLine.ExecuteCommand(
-                    $"{settings.ffmpegExecutable} -loglevel warning -f alsa -channels 2 -sample_rate 48000 -i {inputDevice.deviceId} -t 10 {FFMpegSettings.outputPath}/{fileName}.wav"
+                    $"{settings.ffmpegExecutable} -loglevel warning -f alsa -channels 1 -sample_rate 48000 -i {inputDevice.deviceId} -t 10 {FFMpegSettings.outputPath}/{fileName}.wav"
                 );
                 return fileName + ".wav";
             }
